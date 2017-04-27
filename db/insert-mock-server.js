ecba82ae06d1e55eefdaf723df19c6cad9410520
Jonas Liao <jonhinric2331@hotmail.com>
2017-04-27 21:40:33 +0000

    Implemented factory pattern for 5 insert methods

--- db/insert-mock-server.js
+++ db/insert-mock-server.js
@@ -1,5 +1,5 @@
-const insertTables = require("./insert-tables");
 const db = require("./db-connection");
+const insertTables = require("./insert-tables")(db.connect());
 
 // users: username
 // lists: title, creator_id
@@ -12,10 +12,14 @@ const db = require("./db-connection");
 // insertTables.insertPoint("fourth insertion", "third insertion description", '123, -123', '123,-123', 3);
 // insertTables.insertFavList(3, 3);
 // knex.table('users').pluck('id').then(function(ids) { console.log(ids); });
-
-
 const knex = db.connect();
 
+insertTables.insertUser("Le").then(res => {
+  console.log(res);
+})
+.catch(error => {
+  console.error(error);
+});
 // @Returns res[0].field_to_access
 // const name = "LeBron";
 // knex.select('id')
@@ -58,31 +62,83 @@ const knex = db.connect();
 // GET /lists
 // Return list of lists
 
-function cb(res) {
-  for (let title in res) {
-    if (res.hasOwnProperty(title)) {
-      console.log(res[title].title);
-    }
-  }
-}
-
-knex.select('lists.title')
-.from('lists')
-.then(res => {
-  cb(res);
-  return res;
-})
-.catch(error => {
-  console.error(error);
-});
+// function cb(res) {
+//   for (let title in res) {
+//     if (res.hasOwnProperty(title)) {
+//       console.log(res[title].title);
+//     }
+//   }
+// }
+//
+// knex.select('lists.title')
+// .from('lists')
+// .then(res => {
+//   cb(res);
+//   return res;
+// })
+// .catch(error => {
+//   console.error(error);
+// });
 
 // GET /lists/:list_id/points
 // Return data - points for given list
+// function cb(res) {
+//   for (let field in res) {
+//     if (res.hasOwnProperty(field)) {
+//       console.log("title", res[field].title);
+//       console.log("description", res[field].description);
+//       console.log("image path", res[field].image);
+//       console.log("coordinates", res[field].coordinates);
+//       console.log("list_id", res[field].list_id);
+//     }
+//   }
+// }
+//
+// knex.select('*')
+// .from('points')
+// .join('lists', 'points.list_id', '=', 'lists.id')
+// .where('lists.id', '=', 2)
+// .then(res => {
+//   cb(res);
+//   // console.log(res);
+//   return res;
+// })
+// .catch(error => {
+//   console.error(error);
+// });
+
 // GET /profile/favorites
-// Return favorites for user
+// Return list's title if it is favorited by the user
+// change userId or username to search for that user's favs
+// const userId = 2;
+// knex.select('lists.title')
+// .from('users')
+// .join('lists', 'users.id', '=', 'lists.creator_id')
+// .join('fav_lists', 'lists.id', '=', 'fav_lists.list_id')
+// .where('users.id', '=', userId)
+// .then(res => {
+//   cb(res);
+//   console.log(res);
+// })
+// .catch(error => {
+//   console.error(error);
+// });
+
 // GET /profile/contributions
 // Return contributions
+// SELECT username, users.id FROM users JOIN contributions ON users.id = user_id WHERE users.id = 1;
+// knex.select('username', 'users.id')
+// .from('users')
+// .join('contributions', 'users.id', '=', 'contributions.user_id')
+// .where('users.id', '=', 1)
+// .then(res => {
+//
+//   console.log(res);
+// })
+// .catch(error => {
+//   console.error(error);
+// });
+
+
 // DELETE /points/:point_id
 // Remove point
-
-knex.destroy();
--- db/insert-tables.js
+++ db/insert-tables.js
@@ -4,61 +4,31 @@ const db = require("./db-connection");
 
 // Server decides which function to call
 // module.exports = function()...
-module.exports = {
-  insertUser: (firstName) => {
-    const knex = db.connect();
-    console.log("Inserting into users...");
-    return knex.insert({username: firstName})
-    .into('users')
-    .then((data) => {
-      return knex.destroy();
-    }).catch((error) => {
-      console.error("error running query", error);
-    });
-  },
-  insertList: (title) => {
-    const knex = db.connect();
-    console.log("Inserting into lists...");
-    return knex.insert({title: title})
-    .into('lists')
-    .then((data) => {
-      return knex.destroy();
-    }).catch((error) => {
-      console.error("error running query", error);
-    });
-  },
-  // @params: coord: '40.204942, -123.117063'
-  insertPoint: (title, description, image, coord, listId) => {
-    const knex = db.connect();
-    console.log("Inserting into points...");
-    return knex.insert({title: title, description: description, image: image, coordinates: coord, list_id: listId})
-    .into('points')
-    .then((data) => {
-      return knex.destroy();
-    }).catch((error) => {
-      console.error("error running query", error);
-    });
-  },
-  insertFavList: (listId, userId) => {
-    const knex = db.connect();
-    console.log("Inserting into fav_lists...");
-    return knex.insert({list_id: listId, user_id: userId})
-    .into('fav_lists')
-    .then((data) => {
-      return knex.destroy();
-    }).catch((error) => {
-      console.error("error running query", error);
-    });
-  },
-  insertContributions: (pointId, userId) => {
-    const knex = db.connect();
-    console.log("Inserting into contributions...");
-    return knex.insert({point_id: pointId, user_id: userId})
-    .into('contributions')
-    .then((data) => {
-      return knex.destroy();
-    }).catch((error) => {
-      console.error("error running query", error);
-    });
+module.exports = function(knex) {
+  return {
+    insertUser: (firstName) => {
+      console.log("Inserting into users...");
+      return knex.insert({username: firstName}).into('users');
+    },
+    insertList: (title) => {
+      console.log("Inserting into lists...");
+      return knex.insert({title: title}).into('lists');
+    },
+    // @params: coord: '40.204942, -123.117063'
+    insertPoint: (title, description, image, coord, listId) => {
+      console.log("Inserting into points...");
+      return knex.insert({title: title, description: description, image: image, coordinates: coord, list_id: listId})
+      .into('points');
+    },
+    insertFavList: (listId, userId) => {
+      console.log("Inserting into fav_lists...");
+      return knex.insert({list_id: listId, user_id: userId})
+      .into('fav_lists');
+    },
+    insertContributions: (pointId, userId) => {
+      console.log("Inserting into contributions...");
+      return knex.insert({point_id: pointId, user_id: userId})
+      .into('contributions');
+    }
   }
-}
+};
