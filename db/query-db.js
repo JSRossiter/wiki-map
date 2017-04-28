92f450f926d85a70955ecaf904f5b79f71fb390c
Jonas Liao <jonhinric2331@hotmail.com>
2017-04-28 00:00:36 +0000

    Consolidated query methods into query-db.js

---
+++ db/query-db.js
@@ -0,0 +1,34 @@
+const db = require("./db-connection");
+const insertTables = require("./insert-tables")(db.connect());
+
+module.exports = function(knex) {
+  return {
+    getLists: () => {
+      console.log("Getting all lists");
+      return knex.select('lists.title')
+      .from('lists');
+    },
+    getPoints: (listId) => {
+      console.log("Getting points for ", listId);
+      return knex.select('*')
+      .from('points')
+      .join('lists', 'points.list_id', '=', 'lists.id')
+      .where('lists.id', '=', listId);
+    },
+    getFavoriteLists: (userId) => {
+      console.log("Getting favorite lists for ", userId);
+      return knex.select('lists.title')
+      .from('users')
+      .join('lists', 'users.id', '=', 'lists.creator_id')
+      .join('fav_lists', 'lists.id', '=', 'fav_lists.list_id')
+      .where('users.id', '=', userId);
+    },
+    getContributions: (userId) => {
+      console.log("Getting contributions for ", userId);
+      return knex.select('username', 'users.id')
+      .from('users')
+      .join('contributions', 'users.id', '=', 'contributions.user_id')
+      .where('users.id', '=', userId);
+    }
+  };
+};
